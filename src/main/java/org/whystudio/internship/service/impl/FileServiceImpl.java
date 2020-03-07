994c29345255e54e5c302e793ec3dd4f964404dc
13036480683 <1791482634@qq.com>
2020-03-07 21:09:57 +0800

    完成FileController

--- src/main/java/org/whystudio/internship/service/impl/FileServiceImpl.java
+++ src/main/java/org/whystudio/internship/service/impl/FileServiceImpl.java
@@ -1,10 +1,27 @@
 package org.whystudio.internship.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import io.netty.util.internal.StringUtil;
+import org.graalvm.compiler.lir.alloc.lsra.LinearScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+import org.whystudio.internship.controller.ControllerUtil;
+import org.whystudio.internship.entity.Appraisaldate;
 import org.whystudio.internship.entity.File;
 import org.whystudio.internship.mapper.FileMapper;
 import org.whystudio.internship.service.IFileService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.whystudio.internship.util.JWTTool;
+import org.whystudio.internship.util.QiNiuTool;
+import org.whystudio.internship.vo.JsonResult;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +34,83 @@ import org.springframework.stereotype.Service;
 @Service
 public class FileServiceImpl extends ServiceImpl<FileMapper, File> implements IFileService {
 
+    public static final Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+
+    @Autowired
+    IFileService fileService;
+    @Autowired
+    FileMapper fileMapper;
+
+    @Override
+    public JsonResult uploadToQiNiu(MultipartFile loadFile) {
+
+
+        String type = QiNiuTool.getFileType(loadFile);
+        String url = QiNiuTool.uploadQiNiu(loadFile);
+        String size = String.valueOf(loadFile.getSize());
+        if (url.isEmpty()) {
+            logger.info("上传失败");
+            return ControllerUtil.getFalseResultMsgBySelf("上传失败");
+        }
+        File file = new File();
+        Integer s = Integer.valueOf(size) / 1024;
+        file.setSize(s.toString() + "KB");
+        file.setType(type);
+        file.setUrl(url);
+        try {
+            fileService.save(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ControllerUtil.getFalseResultMsgBySelf("保存到数据库失败");
+        }
+        return ControllerUtil.getSuccessResultBySelf("上传成功");
+
+    }
+
+
+    @Override
+    public JsonResult deleteFileById(Integer id) {
+
+        try {
+            File file = fileMapper.selectById(id);
+            String key = QiNiuTool.urlTokey(file.getUrl());
+            if (QiNiuTool.deleteQiNiuFile(key)) {
+                fileService.removeById(id);
+            } else {
+                return ControllerUtil.getFalseResultMsgBySelf("七牛云删除失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ControllerUtil.getFalseResultMsgBySelf("删除失败");
+        }
+        return ControllerUtil.getSuccessResultBySelf("删除成功");
+
+    }
+
+    @Override
+    public JsonResult deleteFileByUrl(String url) {
+
+        try {
+            String key = QiNiuTool.urlTokey(url);
+            if (QiNiuTool.deleteQiNiuFile(key)) {
+                fileMapper.deleteByUrl(url);
+            } else {
+                return ControllerUtil.getFalseResultMsgBySelf("七牛云删除失败");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ControllerUtil.getFalseResultMsgBySelf("删除失败");
+        }
+        return ControllerUtil.getSuccessResultBySelf("删除成功");
+
+    }
+
+    @Override
+    public JsonResult getFileList() {
+
+        List<File> fileList = fileMapper.selectList(null);
+        return ControllerUtil.getSuccessResultBySelf(fileList);
+
+    }
 }
--- src/test/java/org/whystudio/internship/InternshipApplicationTests.java
+++ src/test/java/org/whystudio/internship/InternshipApplicationTests.java
@@ -1,146 +1,54 @@
 package org.whystudio.internship;
 
-import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.whystudio.internship.entity.Report;
-import org.whystudio.internship.mapper.ReportMapper;
+import org.whystudio.internship.entity.File;
+import org.whystudio.internship.entity.Student;
+import org.whystudio.internship.mapper.FileMapper;
 import org.whystudio.internship.mapper.StudentMapper;
-import org.whystudio.internship.mapper.TeacherMapper;
+import org.whystudio.internship.service.IFileService;
+import org.whystudio.internship.service.IStudentService;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InternshipApplicationTests {
 
 	@Autowired
-	ReportMapper reportMapper;
+	StudentMapper studentMapper;
+	@Autowired
+	FileMapper fileMapper;
+	@Autowired
+	IFileService fileService;
 
 	@Autowired
-	StudentMapper studentMapper;
+	IStudentService iStudentService;
 
 	@Test
 	public void contextLoads() {
+//		List<Student> students = studentMapper.selectList(null);
+//		students.forEach(System.out::print);
+////		for (Student student:students){
+////			System.out.println(student);
+////		}
+//		List<File> files = fileMapper.selectList(null);
+		String url="image.ruankun.xyz/31cb4cbcf643ecbe9d02fe3c5f93a5df";
+//		File file = fileMapper.selectByUrl(url);
+		System.out.println(fileMapper.deleteByUrl(url));
+//		files.forEach(System.out::print);
+//		Student student = new Student();
+//		student.
+//		iStudentService.getOne(new W)
+//		Student student = studentMapper.selectByStuno("123");
+//		System.out.println(student);
+
 	}
 
-	@Test
-	public void rkTest(){
-		List<Integer> asList = Arrays.asList(123, 321, 422, 432, 664, 763);
-		asList.forEach((item) -> {
-			System.out.println(item);
-		});
-		//select by map 多个条件查询
-	}
 
-	/**
-	 *
-	 * 条件构造器查询
-	 * 比如 第一阶段填写了第二阶段没有填写的学生report信息
-	 *
-	 */
-	@Test
-	public void selectByWrapper(){
-		QueryWrapper<Report> queryWrapper = Wrappers.query();
-		queryWrapper.select("stuno","stage1_summary","stage2_summary").isNotNull("stage1_summary")
-				.isNotNull("stage2_summary")
-				.like("stuno", "123");
-		List<Report> reports = reportMapper.selectList(queryWrapper);
-		reports.forEach(System.out::println);
-		System.out.println("记录数:" + reports.size());
-
-		// 查询 第一阶段和第二阶段有没填写的
-		queryWrapper.like(StringUtils.isBlank("stuno"),"stuno","123").isNull("stage1_summary")
-				.or().isNull("stage2_summary")
-				.select(Report.class, info -> !info.getColumn().equals("created"));
-
-		List<Map<String, Object>> maps = reportMapper.selectMaps(queryWrapper);
-		System.out.println("找到第一阶段或者第二阶段没有填写的:");
-		maps.forEach(System.out::println);
-	}
-
-	// 使用lambda
-
-	/**
-	 * lambda 与 上面的方式差不多, 就是不容易写错字段罢了？
-	 */
-	@Test
-	public void testLambda(){
-		LambdaQueryWrapper<Report> lambdaQueryWrapper = Wrappers.lambdaQuery();
-		lambdaQueryWrapper.like(Report::getStuno, "123")
-				.select(Report::getStuno,Report::getStage1Summary,Report::getStage2Summary);
-		List<Map<String, Object>> maps = reportMapper.selectMaps(lambdaQueryWrapper);
-		maps.forEach(System.out::println);
-	}
-	/**
-	 * lambda2
-	 */
-	@Test
-	public void lambda2(){
-		List<Report> list = new LambdaQueryChainWrapper<>(reportMapper)
-				.select(Report::getStuno, Report::getStage1Summary, Report::getStage2Summary)
-				.like(Report::getStuno, "123").list();
-		list.forEach(System.out::println);
-	}
-//	/**
-//	 * 条件构造器自定义sql
-//	 */
-//	@Test
-//	public void modifySql(){
-//
-//	}
-	// 分页查询
-	@Test
-	public void selectPage(){
-		QueryWrapper<Report> queryWrapper = Wrappers.query();
-		queryWrapper.like("stuno", "123");
-		Page<Report> page = new Page<>(1,2);
-		Page<Report> page1 = reportMapper.selectPage(page, queryWrapper);
-		System.out.println("分页情况: size:" + page1.getSize() + ",total:" + page1.getTotal() +
-				", current:" + page1.getCurrent());
-		page1.getRecords().forEach(System.out::println);
-	}
-
-	/**
-	 * 更新
-	 */
-	//uodate中,entity将出现在set中, wrapper将出现在where中
-	/**
-	 * 删除
-	 */
-	// 使用lambdaQueryWrapper即可,很简单
-
-	/**
-	 * ActiveRecord模式
-	 * 真特喵有意思
-	 *  Teacher extends Model<Teacher> 即可
-	 *  还需要保证对应mapper extends baseMapper<>
-	 */
-	// teacher.insert();
-	// teacher.updateById();
-	// teacher.deleteById();
-
-	/**
-	 * 多表联查？
-	 * 使用@Query VO 简单实现即可
-	 */
-
-
-	// 以下是正经测试
-	@Test
-	public void testSelectStudent(){
-		List<Map<String, Object>> maps = studentMapper.listTeachersStudentWithReportStatusAndAppraisalStatus("2010010024");
-		maps.forEach(item -> System.out.println(JSON.toJSONString(item)));
-	}
 }
