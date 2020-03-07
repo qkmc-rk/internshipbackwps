cd7270a1886edf6ee822d2bb39adbfef9cc77776
innovation <578777205@qq.com>
2020-03-07 16:50:37 +0800

    完成newsController

--- src/main/java/org/whystudio/internship/InternshipApplication.java
+++ src/main/java/org/whystudio/internship/InternshipApplication.java
@@ -1,13 +1,11 @@
 package org.whystudio.internship;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2   // 放在有 @Configuration 注解的地方
-@MapperScan("org.whystudio.internship.mapper")
 public class InternshipApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InternshipApplication.class, args);
--- src/test/java/org/whystudio/internship/InternshipApplicationTests.java
+++ src/test/java/org/whystudio/internship/InternshipApplicationTests.java
@@ -1,16 +1,33 @@
 package org.whystudio.internship;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.whystudio.internship.entity.Notification;
+import org.whystudio.internship.mapper.NotificationMapper;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InternshipApplicationTests {
 
+	@Autowired
+	NotificationMapper notificationMapper;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void selectPage(){
+		IPage<Notification> notificationIPage=new Page<>(1,10);
+		notificationIPage=notificationMapper.selectPage(notificationIPage,null);
+		List<Notification> list=notificationIPage.getRecords();
+		list.forEach(System.out :: println);
+	}
 }
